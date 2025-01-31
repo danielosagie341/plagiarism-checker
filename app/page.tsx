d6ab360a735db2abe2556e94b159e7e806a2f8a5
daniel osagie <danielosagie341@gmail.com>
2025-01-31 05:39:59 +0100

    fourth commit

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
     setError('');
 
     try {
-      const response = await fetch(`https://${process.env.NEXT_PUBLIC_API_HOST}/api/check-plagiarism`, {
+      const response = await fetch('http://localhost:5000/api/check-plagiarism', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
